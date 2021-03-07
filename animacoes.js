b30f6d4631ffe53a557a319e9492343770fceb0a
Fernanda Marques <79121074+Fernanda-Marques16@users.noreply.github.com>
2021-03-06 22:25:37 -0300

    Add files via upload

---
+++ animacoes.js
@@ -0,0 +1,8 @@
+function assistir(){
+    window.open("https://www.youtube.com/watch?v=GXrDYboUnnw");
+}
+
+function detalhes(){
+    window.open("https://www.themoviedb.org/tv/46896-the-originals/seasons");
+}
+
